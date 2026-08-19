acd25b9b8e81418104559506f12b267c1af88566
jjjm03299-wq <jjjm03299@gmail.com>
2026-08-19 11:29:57 +0300

    Initialize React app with routing setup

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({
+  routeTree,
+  basepath: "/pin-generator/",
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
